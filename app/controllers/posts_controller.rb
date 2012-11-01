e175dfec366c353c219a9feb357a620b3e7a18e5
Nitin More <nmore2012@gmail.com>
2012-11-01 19:52:10 +0530

    blog with comments and search box

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,80 @@
+class PostsController < ApplicationController
+	def new
+		@post = Post.new
+	end
+
+	def create
+		#only one render allowed
+		#render text: params[:post].inspect
+		#render text: params[:post].sort[0].inspect
+		#render text: params[:post].count.inspect
+		#render text: params[:post].keys.inspect
+		#render text: params[:post].values.inspect
+
+		@post = Post.new(params[:post])
+ 		
+ 		if @post.save
+    		redirect_to action: :show, id: @post.id
+  		else
+    		render 'new'
+  		end
+
+  	end
+
+  	def show
+  		@post = Post.find(params[:id])
+	end
+
+	def index
+
+  		#@posts = Post.all
+
+  		if params[:search]
+	      @posts = Post.find(:all, :conditions => ["title LIKE ?", "%#{params[:search]}%"])
+	    else
+	      @posts = Post.find(:all)
+	    end
+	 
+	end
+
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+
+	def update
+  		 @post = Post.find(params[:id])
+ 
+		  if @post.update_attributes(params[:post])
+		    redirect_to action: :show, id: @post.id
+		  else
+		    render 'edit'
+		  end
+	end
+
+	def destroy
+  		@post = Post.find(params[:id])
+  		@post.destroy
+ 
+  		redirect_to action: :index
+	end
+=begin
+	def search
+	   	    
+	    if params[:search]
+	      @posts = Post.find(:all, :conditions => ["text LIKE ?", "%#{params[:search]}%"])
+	    else
+	      @posts = Post.find(:all)
+	    end
+	 
+	   
+  	end
+
+
+	def search
+    	query = params[:q]
+    	@posts = Post.find(:all, :conditions => ["title = ?", query])
+  	end
+=end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f9ccd71c4b2d8db52f02060498663f756353d7f408796dca4a401c4bc2183ecf6d83075c0eb3e2edc222206cf5c043f6c6113a1d40a50c016bc835cf849bce4b'
